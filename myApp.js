41b14bb71e379dd99f793dbab3b76866ac3796e3
gnorken <64843696+gnorken@users.noreply.github.com>
2022-07-08 22:04:40 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,23 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+let absolutePath = __dirname + "/views/index.html";
 
+const mySecret = process.env["MESSAGE_STYLE"];
+console.log("mySecret", mySecret);
 
+//link the css styles
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  let jsonResponse = { message: "Hello json" };
+  if (mySecret === "uppercase") {
+    jsonResponse.message = jsonResponse.message.toUpperCase();
+  }
+  res.json(jsonResponse);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
